9833ab5ca1720740c03d1b4fca38b56ce497251d
Clay Whitley <pyrino@gmail.com>
2013-06-12 21:58:23 -0400

    Finish sign in

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,25 @@
+class SessionsController < ApplicationController
+
+	def new
+
+	end
+
+	def create
+		user = User.find_by_email(params[:session][:email])
+		if user && user.authenticate(params[:session][:password])
+			#sign in user and redirect to user's show page, sign_in is helper func
+			sign_in user
+			redirect_to user
+		else
+			#create error message and re-render signin form
+			flash.now[:error] = 'Invalid email/password combination'
+			render 'new'
+		end
+	end
+
+	def destroy
+		# call helper func from session helper module
+		sign_out
+		redirect_to root_path
+	end
+end
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -40,6 +40,13 @@ describe "UserPages" do
       it "should create a user" do
         expect { click_button submit }.to change(User, :count).by(1)
       end
+
+      describe "after saving the user" do
+        before { click_button submit }
+
+        it { should have_link('Sign out') }
+        it { should_not have_link('Sign in') }
+      end
     end
   end
 end
