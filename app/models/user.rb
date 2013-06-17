99e63d9d26de6e3a9aacbbb7bfe9972bcb5562bc
Clay Whitley <pyrino@gmail.com>
2013-06-16 23:48:51 -0400

    micropost tests and styling

--- app/models/user.rb
+++ app/models/user.rb
@@ -21,7 +21,7 @@ class User < ActiveRecord::Base
   # passing private method instead of block
   before_save :create_remember_token
 
-  has_many :microposts
+  has_many :microposts, dependent: :destroy
 
   validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
